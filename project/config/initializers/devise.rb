62bae7f08d8a814ece51b3a88a00108f36f13085
Nitin Kumar <snitin315@gmail.com>
2021-07-16 17:55:56 +0530

    Update devise.rb

--- project/config/initializers/devise.rb
+++ project/config/initializers/devise.rb
@@ -52,6 +52,9 @@ Devise.setup do |config|
   # These keys will have whitespace before and after removed upon creating or
   # modifying a user and when used to authenticate or find a user. Default is :email.
   config.strip_whitespace_keys = [:email]
+  
+  # Secret key 
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
 
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
